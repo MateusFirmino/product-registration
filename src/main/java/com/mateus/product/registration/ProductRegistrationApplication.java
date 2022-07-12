4a3c260348a5e272fc9185aed54c86ec19b45948
Mateus Firmino <mateus.ribeiro@totemti.com.br>
2022-07-12 18:58:12 -0400

    Initial commit

---
+++ src/main/java/com/mateus/product/registration/ProductRegistrationApplication.java
@@ -0,0 +1,13 @@
+package com.mateus.product.registration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductRegistrationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProductRegistrationApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/mateus/product/registration/ProductRegistrationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mateus.product.registration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductRegistrationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
