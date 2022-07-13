2770a0b86943f9df834fd8f8a1caaab4c078b92b
Mateus Firmino <mateus.ribeiro@totemti.com.br>
2022-07-13 00:30:27 -0400

    feat: implementa negocio Exception

---
+++ src/main/java/com/mateus/product/registration/exceptions/NegocioException.java
@@ -0,0 +1,7 @@
+package com.mateus.product.registration.exceptions;
+
+public class NegocioException extends RuntimeException {
+    public NegocioException(String message) {
+        super(message);
+    }
+}
